dd4ec3a5c3476efc800d244b3482bdd851fab7d0
Hammas Tahir <hammastahir11@gmail.com>
2022-09-06 01:37:07 +0500

    ScoreBoard

--- Assets/Scrpts/Enemy.cs
+++ Assets/Scrpts/Enemy.cs
@@ -6,7 +6,15 @@ public class Enemy : MonoBehaviour
 {
     [SerializeField] GameObject enemyVFX;
     [SerializeField] Transform parent;
+    [SerializeField] int scorePerHit=10;
+
+
+    ScoreBoard scoreBoard; 
+    void Start() {
+        scoreBoard= FindObjectOfType<ScoreBoard>();
+    }
         void OnParticleCollision(GameObject other) {
+            scoreBoard.IncreaseScore(scorePerHit);
             GameObject vfx = Instantiate(enemyVFX, transform.position,Quaternion.identity);
             vfx.transform.parent=parent;
             Destroy(gameObject);    
